bedc506ddbcccfea57fc25c9c37ff274fb95b634
Clayton Faria <claytonfaria@gmail.com>
2020-10-31 12:57:28 +0800

    deployment adjusments

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,8 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: 'postgres://postgres:@localhost/taskmanagement',
+  url:
+    process.env.DATABASE_URL || 'postgres://postgres:@localhost/taskmanagement',
   entities: ['dist/**/*.entity.js'],
   synchronize: process.env.NODE_ENV !== 'production',
   ssl: {
